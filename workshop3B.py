cd7371963d526b12cb99b42d3e531b7ce5753c35
watcharaphatpw-ai <watcharaphatpw@gmail.com>
2026-07-16 16:35:41 +0700

    Finished Workshop A B

---
+++ workshop3B.py
@@ -0,0 +1,8 @@
+collected_money = 0
+note = 0
+target_money = int(input("จำนวนเงินที่ต้องไปไล่ไถในวันนี้: "))
+while collected_money < target_money :
+    if True: money = int(input("จำนวนเงินที่เก็บได้: "))
+    collected_money += money
+    note += 1
+print("ไถเงินมาได้ทั้งหมด: ",collected_money,"ไล่ไถไปทั้งหมด",note,"ร้าน","Mission Success")
---
+++ workshop3C.py
@@ -0,0 +1,9 @@
+sum = 0
+shops = 0
+police = "police"
+while True: 
+    money = int(input("จำนวนเงินที่ได้: "))
+    money += sum
+    if police.lower() == money: 
+        break
+    print("ตำรวจมา วิ่ง!!")
